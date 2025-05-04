8fce4efd9ac1ded5e319dd21a14845531d267dae
cblte <mail@cbrueggenolte.de>
2025-05-04 14:19:17 +0200

    Refactor index.ts to improve code formatting and add logger middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
 
-const app = new Hono()
+const app = new Hono();
+
+// Middleware hinzufügen
+app.use('*', logger());
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-export default app
+export default app;
